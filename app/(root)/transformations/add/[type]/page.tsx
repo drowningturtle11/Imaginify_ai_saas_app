c00ebe76b15b92539e8b2a79f7886c1582aa8935
drowningturtle11 <anilbadiwal23@gmail.com>
2024-07-11 22:56:51 +0530

    first commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>
+      ddTransformationTypePage
+    </div>
+  )
+}
+
+export default AddTransformationTypePage
